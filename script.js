1254bc8be0a1a830f457f83af413c53c7ab397cc
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2022-09-09 11:53:24 +0600

    Save item in local Storage and Display it in UI from local Storage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getValue = id => {
     const inputField = document.getElementById(id);
     const inputValue = inputField.value;
+    inputField.value = "";
     return inputValue;
 }
 
@@ -11,7 +12,29 @@ const addproduct = () => {
 
 
     displayProduct(productName, productQuantity);
-    localStorage.setItem(productName, productQuantity)
+    //localStorage.setItem(productName, productQuantity)
+    saveItemLocalStorage(productName, productQuantity)
+}
+const getShoppingCartFromLocalStorage = () => {
+
+    let saveCart = localStorage.getItem('cart');
+    let cart = {};
+    if (saveCart) {
+        cart = JSON.parse(saveCart);
+    }
+    return cart
+}
+
+const saveItemLocalStorage = (productName, productQuantity) => {
+
+    const cart = getShoppingCartFromLocalStorage();
+    //add product to the Local Storage
+    cart[productName] = productQuantity;
+    const cartStringified = JSON.stringify(cart)
+
+    //Save item in Local storage 
+    localStorage.setItem('cart', cartStringified)
+
 }
 
 const displayProduct = (productName, productQuantity) => {
@@ -21,3 +44,13 @@ const displayProduct = (productName, productQuantity) => {
     productContainer.appendChild(li)
 
 }
+
+const displayStoredProduct = () => {
+    const cart = getShoppingCartFromLocalStorage();
+    for (const productName in cart) {
+        const productQuantity = cart[productName];
+        console.log(productName, productQuantity);
+        displayProduct(productName, productQuantity)
+    }
+}
+displayStoredProduct();
